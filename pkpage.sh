f665d70f122d5f83522afe928007733f2f5e4086
cmotc <eyedeekay@i2pmail.org>
2016-06-25 10:25:00 -0400

    script sets up from package file

---
+++ pkpage.sh
@@ -0,0 +1,22 @@
+﻿#!/bin/sh
+while getopts ":f:" opt; do
+  case $opt in
+    a)
+      echo "-f using file: $OPTARG"
+      PKG_FILE=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+if [ ! -f PKG_FILE ]; then
+    echo "Package file not found"
+    exit 1
+fi
+PKG_INFO=$(dpkg --info PKG_FILE)
