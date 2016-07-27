387e5e7b31e07b9fef71c414f65311c5c34e3671
idk <idk@devuan.local>
2016-07-27 02:37:41 -0400

    new page.

--- pkpage.sh
+++ pkpage.sh
@@ -3,7 +3,7 @@
 while getopts ":f:" opt; do
   case $opt in
     f)
-      echo "-f using file: $OPTARG" > /dev/null
+      echo "-f using file: $PWD/$OPTARG" > /dev/null
       PKG_FILE=$OPTARG
       ;;
     \?)
@@ -20,16 +20,17 @@ if [ ! -f $PKG_FILE ]; then
     echo "Package file not found" >&2
     exit 1
 fi
-PROCESS_CMD=markdown
+
+cd $PWD
 PKG_INFO=$(dpkg --info $PKG_FILE)
 
 PKGFILEBIN=$(dpkg --info $PKG_FILE | grep 'Package:' | sed -e 's/^[ \t]*//')
 HONE="============="
 printf "%b\n" "\n<div id=\"header\">\n"
-printf "%b\n" "$PROFILEBIN \n $HONE \n" | markdown
+[ ! -z "$PKGFILEBIN" ] && printf "%b\n" "$PKGFILEBIN \n$HONE \n" | tee $PKG_FILE.md | markdown
 PKGFILESRC=$(dpkg --info $PKG_FILE | grep 'Source:' | sed -e 's/^[ \t]*//')
 HTWO="-------------"
-printf "%b\n" "$PROFILESRC \n $HTWO \n" | markdown
+[ ! -z "$PKGFILESRC" ] && printf "%b\n" "$PKGFILESRC \n$HTWO \n" | tee -a $PKG_FILE.md | markdown
 printf "%b\n" "\n</div>\n"
 printf "%b\n" "\n<div id=\"content\">\n"
 PKGFILEVER=$(echo -n "###" && dpkg --info $PKG_FILE | grep 'Version:' | sed -e 's/^[ \t]*//')
@@ -40,11 +41,11 @@ DESCRIPTION_START=$(dpkg --info $PKG_FILE |  grep -n Description |  tr -d '[A-Za
 TOTAL_LINES=$(dpkg --info $PKG_FILE | nl | tail -1 |  tr -d '[A-Za-z]' | tr -d '\-:=. ' )
 TAIL_NUM=$(expr $TOTAL_LINES - $DESCRIPTION_START + 1)
 PKGFILEDSC=$(dpkg --info $PKG_FILE | tail -n $TAIL_NUM | sed -e 's/^[ \t]*//' | sed -e 's/Description:/###Description:\n/')
-printf "%b\n" "$PKGFILEVER" | markdown
-printf "%b\n" "$PKGFILEMAN" | markdown
-printf "%b\n" "$PKGFILEDSC" | markdown
-printf "%b\n" "$PKGFILEDEP" | markdown
-printf "%b\n" "$PKGFILEREC" | markdown
+[ ! -z "$PKGFILEVER" ] && printf "%b\n" "$PKGFILEVER" | tee -a $PKG_FILE.md | markdown
+[ ! -z "$PKGFILEMAN" ] && printf "%b\n" "$PKGFILEMAN" | tee -a $PKG_FILE.md | markdown
+[ ! -z "$PKGFILEDSC" ] && printf "%b\n" "$PKGFILEDSC" | tee -a $PKG_FILE.md | markdown
+[ ! -z "$PKGFILEDEP" ] && printf "%b\n" "$PKGFILEDEP" | tee -a $PKG_FILE.md | markdown
+[ ! -z "$PKGFILEREC" ] && printf "%b\n" "$PKGFILEREC" | tee -a $PKG_FILE.md | markdown
 printf "%b\n" "\n</div>\n"
 printf "%b\n" "\n<div id=\"sidebar\">\n"
 PKGFILEPRV=$(dpkg --info $PKG_FILE | grep 'Provides:' | sed -e 's/^[ \t]*//' | sed -e 's/Provides:/###Provides:\n  */')
@@ -53,9 +54,9 @@ PKGFILEPRI=$(echo -n "####" && dpkg --info $PKG_FILE | grep 'Priority:' | sed -e
 PKGFILESIZ=$(echo -n "####" && dpkg --info $PKG_FILE | grep 'Installed-Size:' | sed -e 's/^[ \t]*//')
 PKG_URL=$(dpkg --info $PKG_FILE | grep 'Homepage:' | sed -e 's/Homepage:/ /' | sed -e 's/^[ \t]*//') #)
 PKGFILEURL=$(dpkg --info $PKG_FILE | grep 'Homepage:' | sed -e 's/^[ \t]*//' | sed -e 's/Homepage:/Homepage:[/' | tr "\n" " " && echo "]($PKG_URL)")
-printf "%b\n" "$PKGFILEURL" | markdown
-printf "%b\n" "$PKGFILEPRV" | markdown
-printf "%b\n" "$PKGFILESEC" | markdown
-printf "%b\n" "$PKGFILESIZ" | markdown
-printf "%b\n" "$PKGFILEPRI" | markdown
+[ ! -z "$PKGFILEURL" ] && printf "%b\n" "$PKGFILEURL" | tee -a $PKG_FILE.md | markdown
+[ ! -z "$PKGFILEPRV" ] && printf "%b\n" "$PKGFILEPRV" | tee -a $PKG_FILE.md | markdown
+[ ! -z "$PKGFILESEC" ] && printf "%b\n" "$PKGFILESEC" | tee -a $PKG_FILE.md | markdown
+[ ! -z "$PKGFILESIZ" ] && printf "%b\n" "$PKGFILESIZ" | tee -a $PKG_FILE.md | markdown
+[ ! -z "$PKGFILEPRI" ] && printf "%b\n" "$PKGFILEPRI" | tee -a $PKG_FILE.md | markdown
 printf "%b\n" "\n</div>\n"
